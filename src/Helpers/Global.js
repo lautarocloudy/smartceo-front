3884249f319987f82814cdb1e41e84c26f1f8b78
unknown <lautarorcloudy@gmail.com>
2024-05-17 16:42:19 -0300

    update

--- src/Helpers/Global.js
+++ src/Helpers/Global.js
@@ -1,6 +1,6 @@
 const Global =  {
     // url: "http://localhost:3000/api/"
-    url:"https://north-baires-paris-back.vercel.app/api/"
+    url:"https://smartceo-back.vercel.app/api/"
    }
    
    export default Global
